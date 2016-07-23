f8f2a6877720b3e52d66b46107a22eb89a08ffec
Linh <mr.lynk92@gmail.com>
2016-07-24 05:53:59 +0700

    Hompage demo

---
+++ app/assets/js/fire.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($){
+	Hapro.Homepage.init();
+})
---
+++ app/assets/js/main.js
@@ -0,0 +1,93 @@
+var Hapro = Hapro || {};
+
+Hapro = {
+	Homepage:{
+		init: function(){
+			this.bannerSlider();
+			this.newsSlider('#business-news');
+			this.newsSlider('#market-news');
+			this.brandsSlider();
+		},
+		bannerSlider : function(){
+			$("#home-slider").slick({
+				slidesToScroll: 1,
+				asNavFor: "#home-slider-nav",
+				prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+				nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></button>'
+				
+			});
+			$("#home-slider-nav").slick({
+				asNavFor: "#home-slider",
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				arrows: false,
+				focusOnSelect: true,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings:{
+							slidesToShow: 3 
+						}
+					},
+					{
+						breakpoint: 767,
+						settings:{
+							slidesToShow: 3
+						}
+					},
+					{
+						breakpoint: 480,
+						settings:{
+							slidesToShow: 1
+						}
+					}
+				]
+			})
+		},
+		newsSlider: function(element){
+			
+			$(element).slick({
+				slidesToScroll: 1,
+				slidesToShow: 2,
+				arrows: false,
+				responsive: [
+					{
+						breakpoint: 480,
+						settings:{
+							slidesToShow: 1
+						}
+					}
+				]
+			})
+		},
+		brandsSlider: function(){
+			$("#brands-slider").slick({
+				slidesToScroll: 1,
+				slidesToShow: 6,
+				prevArrow: $("#slider-prev"),
+				nextArrow: $("#slider-next"),
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings:{
+							slidesToShow: 4 
+						}
+					},
+					{
+						breakpoint: 767,
+						settings:{
+							slidesToShow: 3
+						}
+					},
+					{
+						breakpoint: 480,
+						settings:{
+							slidesToShow: 2
+						}
+					}
+				]
+			})
+			
+		}
+	}
+}
