0fce414d7b42be712af106a281d9d0fdd15a3635
trungnt3 <trungnt3@smartosc.com>
2016-09-15 15:03:47 +0700

    update css

--- wp-content/themes/hapro/header.php
+++ wp-content/themes/hapro/header.php
@@ -14,7 +14,7 @@
 <head>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatiable" cotent="IE=Edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="apple-touch-icon" type="image/png" href="apple-touch-icon.png">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
